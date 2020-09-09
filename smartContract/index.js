25fe097323f7fc6273e283b1f0e86a775d1da519
thiagostival <thiagofilipestival5@gmail.com>
2020-09-09 12:51:49 -0300

    Smart Contract da rede Blockchain

---
+++ smartContract/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const HealthPacientesContract = require('./lib/health-pacientes-contract');
+
+module.exports.HealthPacientesContract = HealthPacientesContract;
+module.exports.contracts = [ HealthPacientesContract ];
